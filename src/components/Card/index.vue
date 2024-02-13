547b57df3b8a48284df5c0ebbffb37161c74d53a
zlsk1 <3506466428@qq.com>
2024-02-14 00:02:36 +0800

    更改Card组件为首字母大写

---
+++ src/components/Card/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card-wrap">
+    <slot />
+    <div class="card-footer">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  imgUrl: { type: String, default: '' },
+  imgHeight: { type: String, default: '140px' }
+})
+</script>
+
+<style lang="scss" scoped>
+.card-wrap {
+  position: relative;
+  .card-img {
+    width: 100%;
+  }
+  .card-footer {
+    margin-top: 6px;
+  }
+}
+</style>
