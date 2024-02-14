f84b12ec3ad9d2fe819c5aab3ea8a51dd4e385d3
zlsk1 <3506466428@qq.com>
2024-02-14 14:16:45 +0800

    修改axios配置

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,9 +2,10 @@ import axios from 'axios'
 
 const http = axios.create({
   baseURL: 'http://localhost:3000',
-  withCredentials: true,
+  // withCredentials: true,
   timeout: 10000
 })
+http.defaults.withCredentials = true
 
 axios.interceptors.request.use(config => {
   // 在发送请求之前做些什么
